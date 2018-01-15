b896c612ff0f1ec44f7c02fdda9258f75d21a97e
jerrycheese <thinkjerry@icloud.com>
2018-01-15 15:35:21 +0800

    new short-cmds

--- scripts/short-cmd.sh
+++ scripts/short-cmd.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cur=$PWD
 machine=''
+cmd=$@
+
+if [ -z "$cmd" ]
+then
+    echo "You should input some cmds!"
+    exit
+fi
+
 if [[ ${PWD:0:6} = '/data-' ]]
 then
     t=${PWD#*/data-}
@@ -9,18 +17,15 @@ then
     cur="/$t"
 fi
 
-cmd=$1
-cmd=${cmd#*;}
-cmd=${cmd#*sc }
 if [[ ${cmd:0:2} = '-i' ]]
 then
     app=${cmd#-i *}
     container_name="${machine}${app}1"
     echo "You might want: $cur"
-    docker exec -it $container_name /bin/sh
+    #docker exec -it $container_name /bin/sh
 else
     app=${cmd%% *}
     container_name="${machine}${app}1"
     echo "[${container_name} $cur]$ $cmd"
-    docker exec -it $container_name /bin/sh  -c "cd $cur && $cmd"
+    #docker exec -it $container_name /bin/sh  -c "cd $cur && $cmd"
 fi
